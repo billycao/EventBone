502c44bff7a1892fd23a29d4fae6962e5ccef6a2
Billy Cao <billy.cao@ucla.edu>
2011-06-29 23:07:09 -0700

    Calendar helper functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// Calendar functions
+function gcalLogin() {
+  scope = "https://www.google.com/calendar/feeds/";
+  var token = google.accounts.user.login(scope);
+}
+
+function gcalLogout() {
+  google.accounts.user.logout();
+}
+
+function gcalSetup() {
+  gcalService = new google.gdata.calendar.CalendarService('EventBone-1');
+  gcalLogin();
+}
+
+
 $(function(){
 
 });
